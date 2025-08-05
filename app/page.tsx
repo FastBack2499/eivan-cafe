d239d3259e833fc8457c6f53746744ed361b1704
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-08-05 17:59:04 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import Navbar from './components/Navbar'
 import Menu from './components/Menu'
 import Footer from './components/Footer'
+import PageLoader from './components/PageLoader'
 
 const page = () => {
   return (
-      <div className='flex items-center justify-center flex-col w-full font-vazir'>
-        <Navbar/>
-        <Menu/>
-        <Footer/>
-      </div>
+
+      <PageLoader>
+
+        <div className='flex items-center justify-center flex-col w-full font-vazir bg-orange-300/20'>
+
+          <Navbar/>
+          <Menu/>
+          <Footer/>
+          
+        </div>
+
+      </PageLoader>
+
   )
 }
 
