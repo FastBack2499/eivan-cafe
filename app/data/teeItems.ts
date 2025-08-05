eca5b311472e448fd8ed5fd3c0d8fff2e5d8eed8
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-08-05 17:57:21 +0330

    Update teeItems.ts

--- app/data/teeItems.ts
+++ app/data/teeItems.ts
@@ -14,5 +14,25 @@ export const teeItems: {
       id: 1,
       title: 'چای',
       price: 50
-    }
+    },
+    {
+      id: 2,
+      title: 'گل گاوزبان',
+      price: 69
+    },
+    {
+      id: 3,
+      title: 'به لیمو',
+      price: 69
+    },
+    {
+      id: 4,
+      title: 'آویشن',
+      price: 69
+    },
+    {
+      id: 5,
+      title: 'دمنوش دلخواه',
+      price: 69
+    },
   ]
