285330f4251502053d3b373f3f7472c59842c397
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-08-07 00:06:34 +0330

    Update cakeItems.ts

--- app/data/cakeItems.ts
+++ app/data/cakeItems.ts
@@ -1,4 +1,15 @@
-export const cakeItems = [
+export const cakeItems: {
+    id: number,
+    title: string;
+    description?: string;
+    picture?: string;
+    price?: number;
+    prices?: {
+      id: number,
+      priceTitle: string,
+      priceNum: number,
+    }[]
+  }[] = [
   {
     id: 1,
     title: 'کوکی',
