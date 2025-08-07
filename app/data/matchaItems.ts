4a75c1d17ce8deb6c88aabbaf2975191591bd431
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-08-07 18:41:16 +0330

    Add

---
+++ app/data/matchaItems.ts
@@ -0,0 +1,23 @@
+export const matchaItems: {
+    id: number,
+    title: string;
+    description?: string;
+    picture?: string;
+    price?: number;
+    prices?: {
+      id: number,
+      priceTitle: string,
+      priceNum: number,
+    }[]
+  }[] = [
+  {
+    id: 1,
+    title: 'ماچا لاته',
+      price: 114
+  },
+    {
+    id: 2,
+    title: 'آیس ماچا لاته',
+      price: 114
+  },
+]
