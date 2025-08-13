7e2eb7e967136ed28f22f6184c3459e1f3a7f66b
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-08-13 19:16:01 +0330

    Update mocktailItems.ts

--- app/data/mocktailItems.ts
+++ app/data/mocktailItems.ts
@@ -1,1 +1,23 @@
-
+export const mocktailItems: {
+    id: number,
+    title: string;
+    description?: string;
+    picture?: string;
+    price?: number;
+    prices?: {
+      id: number,
+      priceTitle: string,
+      priceNum: number,
+    }[]
+  }[] = [
+  {
+    id: 1,
+    title: 'اورنج پانچ',
+    price: 62
+  },
+  {
+    id: 2,
+    title: 'لیموناد',
+    price: 62
+  },
+]
