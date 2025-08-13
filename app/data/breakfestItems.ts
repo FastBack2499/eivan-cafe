0dc947de5447c89301982357df2140cf6a2ef0b9
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-08-13 17:12:44 +0330

    Update breakfestItems.ts

--- app/data/breakfestItems.ts
+++ app/data/breakfestItems.ts
@@ -1,1 +1,18 @@
-
+export const breakfestItems: {
+    id: number,
+    title: string;
+    description?: string;
+    picture?: string;
+    price?: number;
+    prices?: {
+      id: number,
+      priceTitle: string,
+      priceNum: number,
+    }[]
+  }[] = [
+  {
+    id: 1,
+    title: 'املت',
+    price: 85
+  },
+]
